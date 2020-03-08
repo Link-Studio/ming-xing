1d0c5ea70dbb9375c017b08730b82271d5aec09d
Linkstudio <qwqw1472001@gmail.com>
2020-03-08 14:08:31 +0800

    git push

--- _meta.php
+++ _meta.php
@@ -3,18 +3,18 @@
 
 <!-- SEO 一般瀏覽器用 -->
 <title>楠堂文藝工作室</title>
-<meta name="title" content="明行">
-<meta name="keywords"  content="明行,篆刻,手工印章,手作印章,修復,裝裱,書畫,書法,國畫,水墨">
+<meta name="title" content="楠堂文藝工作室">
+<meta name="keywords"  content="楠堂文藝工作室,篆刻,手工印章,手作印章,修復,裝裱,書畫,書法,國畫,水墨">
 <meta name="description" content="描述文案200字以內" >
 <meta name="author" content="China-motor-IT-team">
 
 <!-- SEO Google Plus用 -->
-<meta itemprop="name" content="明行">
+<meta itemprop="name" content="楠堂文藝工作室">
 <meta itemprop="image" content="images/SEO.jpg"><!--找一張好看的圖片-->
 <meta itemprop="description" content="描述文案200字以內">
 
 <!-- SEO Facebook用 -->
-<meta property="og:title" content="明行" />
+<meta property="og:title" content="楠堂文藝工作室" />
 <meta property="og:url" content="https://www.mitsubishi-motors.com.tw/" /><!--更換為網址-->
 <meta property="og:type" content="website"/>
 <meta property="og:image" content="images/SEO.jpg" /><!--找一張好看的圖片-->
@@ -22,7 +22,7 @@
 <meta property="article:published_time" content="2019-09-01T05:59:00+01:00" /><!--版本日期-->
 
 <!-- SEO Twitter 用 -->
-<meta name="twitter:title" content="明行">
+<meta name="twitter:title" content="楠堂文藝工作室">
 <meta name="twitter:card" content="images/SEO.jpg"><!--找一張好看的圖片-->
 <meta name="twitter:description" content="描述文案200字以內">
 <meta name="twitter:creator" content="China-motor-IT-team">

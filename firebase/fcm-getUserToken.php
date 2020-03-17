9bfad5f7bb765e75c7a1c93dfdfe74213795a794
Linkstudio <qwqw1472001@gmail.com>
2020-03-17 19:20:32 +0800

    2020

--- firebase/fcm-getUserToken.php
+++ firebase/fcm-getUserToken.php
@@ -6,13 +6,14 @@
     <title>Firebase</title>
 </head>
 <body>
+<div id="token"></div>
 <!-- The core Firebase JS SDK is always required and must be listed first -->
 <script src="https://www.gstatic.com/firebasejs/7.11.0/firebase-app.js"></script>
 
 <!-- TODO: Add SDKs for Firebase products that you want to use
      https://firebase.google.com/docs/web/setup#available-libraries -->
 <script src="https://www.gstatic.com/firebasejs/7.11.0/firebase-analytics.js"></script>
-<script src="https://www.gstatic.com/firebasejs/7.11.0/firebase-message.js"></script>
+<script src="https://www.gstatic.com/firebasejs/7.11.0/firebase-messaging.js"></script>
 
 <script>
   // Your web app's Firebase configuration
@@ -29,6 +30,26 @@
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
+
+
+  messaging.usePublicVapidKey("BMn8SxvdaxEaK45f0bx_e3Uu0KASQsUuLGIf4W_AuT7mA37qiGFgEESyfhDmkCgBTCuDALAuVVinETKfyAYgS4s");
+  messaging.getToken().then((currentToken) => {
+  if (currentToken) {
+    sendTokenToServer(currentToken);
+    updateUIForPushEnabled(currentToken);
+  } else {
+    // Show permission request.
+    console.log('No Instance ID token available. Request permission to generate one.');
+    // Show permission UI.
+    updateUIForPushPermissionRequired();
+    setTokenSentToServer(false);
+  }
+}).catch((err) => {
+  console.log('An error occurred while retrieving token. ', err);
+  showToken('Error retrieving Instance ID token. ', err);
+  setTokenSentToServer(false);
+});
+
 </script>
 
 

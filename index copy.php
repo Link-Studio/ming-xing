af9aaca50415452a423d482d537c66dfad26499e
Linkstudio <qwqw1472001@gmail.com>
2020-04-27 13:48:57 +0800

    20200417 13:48

---
+++ index copy.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+<head>
+<?php include'_meta.php'; ?>
+<!--?php include'_schema.php'; ?-->
+<!--?php include'_gtag.php'; ?-->
+
+<!-- CSS -->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<!--<link rel="stylesheet" type="text/css" href="css/slick.css">-->
+<!--<link rel="stylesheet" type="text/css" href="css/slick-theme.css">-->
+<!--<link rel="stylesheet" type="text/css" href="css/animate.min.css">-->
+<!--<link rel="stylesheet" type="text/css" href="css/will-show.min.css">-->
+<style>
+
+</style>
+</head>
+<body>
+<?php include'./layout/N0001.php'; ?>
+<?php include'_GTM.php'; ?>
+<!-- HTML --> 
+
+
+
+
+    
+    
+    
+    
+
+<!-- JS --> 
+<script type="text/javascript" charset="utf-8" src="js/jquery-3.2.1.min.js"></script>
+<!--<script type="text/javascript" charset="utf-8" src="js/jquery.lazy.min.js"></script> -->
+<!--<script type="text/javascript" charset="utf-8" src="js/getUrlPara.js"></script> -->
+<script type="text/javascript" charset="utf-8" src="js/bootstrap.bundle.min.js"></script> 
+<!--<script type="text/javascript" charset="utf-8" src="js/slick.min.js" ></script> -->
+<!--<script type="text/javascript" charset="utf-8" src="js/AJAX-example.js"> </script>--> 
+<!--<script type="text/javascript" charset="utf-8" src="js/form-check-function.js"></script>-->
+<!--<script type="text/javascript" charset="utf-8" src="js/will-show.js"></script>-->
+
+</body>
+<script>
+pageView('首頁2','index2.php')
+</script>
+</html>

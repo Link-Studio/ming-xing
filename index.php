3fa61e27b903922769bb9f097918256e6e426dd3
Linkstudio <qwqw1472001@gmail.com>
2020-04-27 15:33:35 +0800

    20200427 15:33

--- index.php
+++ index.php
@@ -1,112 +1,115 @@
 <!doctype html>
 <html>
+
 <head>
-<?php include'_meta.php'; ?>
-<!--?php include'_schema.php'; ?-->
-<?php include'_gtag.php'; ?>
+    <?php include'_meta.php'; ?>
+    <!--?php include'_schema.php'; ?-->
+    <?php include'_gtag.php'; ?>
 
-<!-- CSS -->
-<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="css/slick.css">
-<link rel="stylesheet" type="text/css" href="css/slick-theme.css">
-<link rel="stylesheet" type="text/css" href="css/animate.min.css">
-<link rel="stylesheet" type="text/css" href="css/will-show.css">
-<link rel="stylesheet" type="text/css" href="css/style.css">
-<style>
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/slick.css">
+    <link rel="stylesheet" type="text/css" href="css/slick-theme.css">
+    <link rel="stylesheet" type="text/css" href="css/animate.min.css">
+    <link rel="stylesheet" type="text/css" href="css/will-show.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <style>
 
-</style>
+    </style>
 </head>
+
 <body>
-<!-- HTML --> 
-<?php include'./inc/nav.html'; ?>
+    <!-- HTML -->
+    <?php include'./inc/nav.html'; ?>
 
 
-<section id="C0001" style="margin-top:70px">
-            <div>
-                <a href="#" target="_self">
-                    <img data-lazy="./images/C0001/banner_pc_01.png" alt="" class="d-none d-lg-block img-fluid">
-                    <img data-lazy="./images/C0001/banner_mobile_01.png" alt="" class="d-block d-lg-none img-fluid">
-                </a>
-            </div>
-            <div>
-                <a href="#" target="_self">
-                    <img data-lazy="./images/C0001/banner_pc_02.png" alt="" class="d-none d-lg-block img-fluid">
-                    <img data-lazy="./images/C0001/banner_mobile_02.png" alt="" class="d-block d-lg-none img-fluid">
-                </a>
-            </div>
-            <div>
-                <a href="#" target="_self">
-                    <img data-lazy="./images/C0001/banner_pc_03.png" alt="" class="d-none d-lg-block img-fluid">
-                    <img data-lazy="./images/C0001/banner_mobile_03.png" alt="" class="d-block d-lg-none img-fluid">
-                </a>
-            </div>
-            <div>
-                <a href="#" target="_self">
-                    <img data-lazy="./images/C0001/banner_pc_04.png" alt="" class="d-none d-lg-block img-fluid">
-                    <img data-lazy="./images/C0001/banner_mobile_04.png" alt="" class="d-block d-lg-none img-fluid">
-                </a>
-            </div>
-            <div>
-                <a href="#" target="_self">
-                    <img data-lazy="./images/C0001/banner_pc_05.png" alt="" class="d-none d-lg-block img-fluid">
-                    <img data-lazy="./images/C0001/banner_mobile_05.png" alt="" class="d-block d-lg-none img-fluid">
-                </a>
-            </div>
-            <div>
-                <a href="#" target="_self">
-                    <img data-lazy="./images/C0001/banner_pc_06.png" alt="" class="d-none d-lg-block img-fluid">
-                    <img data-lazy="./images/C0001/banner_mobile_06.png" alt="" class="d-block d-lg-none img-fluid">
-                </a>
-            </div>
-            <div>
-                <a href="#" target="_self">
-                    <img data-lazy="./images/C0001/banner_pc_07.png" alt="" class="d-none d-lg-block img-fluid">
-                    <img data-lazy="./images/C0001/banner_mobile_07.png" alt="" class="d-block d-lg-none img-fluid">
-                </a>
-            </div>
-            <div>
-                <a href="#" target="_self">
-                    <img data-lazy="./images/C0001/banner_pc_08.png" alt="" class="d-none d-lg-block img-fluid">
-                    <img data-lazy="./images/C0001/banner_mobile_08.png" alt="" class="d-block d-lg-none img-fluid">
-                </a>
-            </div>
-            <div>
-                <a href="#" target="_self">
-                    <img data-lazy="./images/C0001/banner_pc_09.png" alt="" class="d-none d-lg-block img-fluid">
-                    <img data-lazy="./images/C0001/banner_mobile_09.png" alt="" class="d-block d-lg-none img-fluid">
-                </a>
-            </div>
-            <div>
-                <a href="#" target="_self">
-                    <img data-lazy="./images/C0001/banner_pc_10.png" alt="" class="d-none d-lg-block img-fluid">
-                    <img data-lazy="./images/C0001/banner_mobile_10.png" alt="" class="d-block d-lg-none img-fluid">
-                </a>
-            </div>
-            <div>
-                <a href="#" target="_self">
-                    <img data-lazy="./images/C0001/banner_pc_01.png" alt="" class="d-none d-lg-block img-fluid">
-                    <img data-lazy="./images/C0001/banner_mobile_01.png" alt="" class="d-block d-lg-none img-fluid">
-                </a>
-            </div>
+    <section id="C0001" style="margin-top:70px">
+        <div>
+            <a href="#" target="_self">
+                <img data-lazy="./images/C0001/banner_pc_01.png" alt="" class="d-none d-lg-block img-fluid">
+                <img data-lazy="./images/C0001/banner_mobile_01.png" alt="" class="d-block d-lg-none img-fluid">
+            </a>
+        </div>
+        <div>
+            <a href="#" target="_self">
+                <img data-lazy="./images/C0001/banner_pc_02.png" alt="" class="d-none d-lg-block img-fluid">
+                <img data-lazy="./images/C0001/banner_mobile_02.png" alt="" class="d-block d-lg-none img-fluid">
+            </a>
+        </div>
+        <div>
+            <a href="#" target="_self">
+                <img data-lazy="./images/C0001/banner_pc_03.png" alt="" class="d-none d-lg-block img-fluid">
+                <img data-lazy="./images/C0001/banner_mobile_03.png" alt="" class="d-block d-lg-none img-fluid">
+            </a>
+        </div>
+        <div>
+            <a href="#" target="_self">
+                <img data-lazy="./images/C0001/banner_pc_04.png" alt="" class="d-none d-lg-block img-fluid">
+                <img data-lazy="./images/C0001/banner_mobile_04.png" alt="" class="d-block d-lg-none img-fluid">
+            </a>
+        </div>
+        <div>
+            <a href="#" target="_self">
+                <img data-lazy="./images/C0001/banner_pc_05.png" alt="" class="d-none d-lg-block img-fluid">
+                <img data-lazy="./images/C0001/banner_mobile_05.png" alt="" class="d-block d-lg-none img-fluid">
+            </a>
+        </div>
+        <div>
+            <a href="#" target="_self">
+                <img data-lazy="./images/C0001/banner_pc_06.png" alt="" class="d-none d-lg-block img-fluid">
+                <img data-lazy="./images/C0001/banner_mobile_06.png" alt="" class="d-block d-lg-none img-fluid">
+            </a>
+        </div>
+        <div>
+            <a href="#" target="_self">
+                <img data-lazy="./images/C0001/banner_pc_07.png" alt="" class="d-none d-lg-block img-fluid">
+                <img data-lazy="./images/C0001/banner_mobile_07.png" alt="" class="d-block d-lg-none img-fluid">
+            </a>
+        </div>
+        <div>
+            <a href="#" target="_self">
+                <img data-lazy="./images/C0001/banner_pc_08.png" alt="" class="d-none d-lg-block img-fluid">
+                <img data-lazy="./images/C0001/banner_mobile_08.png" alt="" class="d-block d-lg-none img-fluid">
+            </a>
+        </div>
+        <div>
+            <a href="#" target="_self">
+                <img data-lazy="./images/C0001/banner_pc_09.png" alt="" class="d-none d-lg-block img-fluid">
+                <img data-lazy="./images/C0001/banner_mobile_09.png" alt="" class="d-block d-lg-none img-fluid">
+            </a>
+        </div>
+        <div>
+            <a href="#" target="_self">
+                <img data-lazy="./images/C0001/banner_pc_10.png" alt="" class="d-none d-lg-block img-fluid">
+                <img data-lazy="./images/C0001/banner_mobile_10.png" alt="" class="d-block d-lg-none img-fluid">
+            </a>
+        </div>
+        <div>
+            <a href="#" target="_self">
+                <img data-lazy="./images/C0001/banner_pc_01.png" alt="" class="d-none d-lg-block img-fluid">
+                <img data-lazy="./images/C0001/banner_mobile_01.png" alt="" class="d-block d-lg-none img-fluid">
+            </a>
+        </div>
 
-        </section>
-    
-    
-    
-    
+    </section>
 
-<!-- JS --> 
-<script type="text/javascript" charset="utf-8" src="js/jquery-3.2.1.min.js"></script>
-<!--<script type="text/javascript" charset="utf-8" src="js/jquery.lazy.min.js"></script> -->
-<!--<script type="text/javascript" charset="utf-8" src="js/getUrlPara.js"></script> -->
-<script type="text/javascript" charset="utf-8" src="js/bootstrap.bundle.min.js"></script> 
-<script type="text/javascript" charset="utf-8" src="js/slick.min.js" ></script>
-<!--<script type="text/javascript" charset="utf-8" src="js/AJAX-example.js"> </script>--> 
-<!--<script type="text/javascript" charset="utf-8" src="js/form-check-function.js"></script>-->
-<script type="text/javascript" charset="utf-8" src="js/will-show.js"></script>
-<script type="text/javascript" charset="utf-8" src="js/all.js"></script>
+
+
+
+
+    <!-- JS -->
+    <script type="text/javascript" charset="utf-8" src="js/jquery-3.2.1.min.js"></script>
+    <!--<script type="text/javascript" charset="utf-8" src="js/jquery.lazy.min.js"></script> -->
+    <!--<script type="text/javascript" charset="utf-8" src="js/getUrlPara.js"></script> -->
+    <script type="text/javascript" charset="utf-8" src="js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" charset="utf-8" src="js/slick.min.js"></script>
+    <!--<script type="text/javascript" charset="utf-8" src="js/AJAX-example.js"> </script>-->
+    <!--<script type="text/javascript" charset="utf-8" src="js/form-check-function.js"></script>-->
+    <script type="text/javascript" charset="utf-8" src="js/will-show.js"></script>
+    <script type="text/javascript" charset="utf-8" src="js/all.js"></script>
 </body>
 <script>
-pageView('首頁','index.php');
+pageView('首頁', 'index.php');
 </script>
+
 </html>

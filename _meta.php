38477b0c3c21e6cccfcbe2b3e9e0fb3df12cff02
Linkstudio <qwqw1472001@gmail.com>
2020-04-27 13:40:32 +0800

    20200427 13:40

--- _meta.php
+++ _meta.php
@@ -4,27 +4,27 @@
 <!-- SEO 一般瀏覽器用 -->
 <title>楠堂文藝工作室</title>
 <meta name="title" content="楠堂文藝工作室">
-<meta name="keywords"  content="楠堂文藝工作室,篆刻,手工印章,手作印章,修復,裝裱,書畫,書法,國畫,水墨,手工框">
-<meta name="description" content="描述文案200字以內" >
+<meta name="keywords"  content="楠堂文藝工作室,篆刻,手工印章,手作印章,修復,裝裱,書畫,書法,國畫,水墨,手工框,裝裱推薦,卷軸">
+<meta name="description" content="楠堂文藝工作室，主要服務為精緻裝裱、修復、手工篆刻為主。" >
 <meta name="author" content="China-motor-IT-team">
 
 <!-- SEO Google Plus用 -->
 <meta itemprop="name" content="楠堂文藝工作室">
 <meta itemprop="image" content="images/SEO.jpg"><!--找一張好看的圖片-->
-<meta itemprop="description" content="描述文案200字以內">
+<meta itemprop="description" content="楠堂文藝工作室，主要服務為精緻裝裱、修復、手工篆刻為主。">
 
 <!-- SEO Facebook用 -->
 <meta property="og:title" content="楠堂文藝工作室" />
-<meta property="og:url" content="https://www.mitsubishi-motors.com.tw/" /><!--更換為網址-->
+<meta property="og:url" content="https://nan-tang.herokuapp.com/" /><!--更換為網址-->
 <meta property="og:type" content="website"/>
 <meta property="og:image" content="images/SEO.jpg" /><!--找一張好看的圖片-->
-<meta property="og:description" content="描述文案200字以內"  />
+<meta property="og:description" content="楠堂文藝工作室，主要服務為精緻裝裱、修復、手工篆刻為主。"  />
 <meta property="article:published_time" content="2019-09-01T05:59:00+01:00" /><!--版本日期-->
 
 <!-- SEO Twitter 用 -->
 <meta name="twitter:title" content="楠堂文藝工作室">
 <meta name="twitter:card" content="images/SEO.jpg"><!--找一張好看的圖片-->
-<meta name="twitter:description" content="描述文案200字以內">
+<meta name="twitter:description" content="楠堂文藝工作室，主要服務為精緻裝裱、修復、手工篆刻為主。">
 <meta name="twitter:creator" content="China-motor-IT-team">
 
 <!-- 瀏覽器icon -->

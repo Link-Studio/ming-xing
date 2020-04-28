f1ec2f9dcfce6faf8605ab617dc245c20ab8b50f
Linkstudio <qwqw1472001@gmail.com>
2020-04-28 16:31:44 +0800

    20200428 16:31

--- instagram.php
+++ instagram.php
@@ -72,9 +72,10 @@ $(document).ready(function() {
                     // console.log('串接API中');
                 },
                 success: function(data) {
+                    console.log(data);
                     // console.log('串接API成功');
                     // console.log(data['html']);
-                    $('.card-columns').append('<div class="card">'+data['html']+'</div> ')
+                    // $('.card-columns').append('<div class="card">'+data['html']+'</div> ');
                 },
                 error: function() {
                     console.log('串接API失敗');
